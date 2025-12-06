80bb7df70d30888ca66fa51d9721feb20574807d
smattechnology <abirhasan.devloper@gmail.com>
2025-12-07 03:17:42 +0600

    test

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"], // build from index.ts
-  format: ["esm"],
+  format: ["esm", "cjs"],
   dts: true,
   sourcemap: true,
   clean: true,
